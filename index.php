c44231cad585b2d86ee8ee4ee7638b1384ddb799
Uche Uba <ucheuba@Uches-MacBook-Pro.local>
2021-01-29 00:00:23 -0800

    Added the search by first name functionality

--- index.php
+++ index.php
@@ -20,38 +20,65 @@ inner join customers
 on invoices.customer_id = customers.id
 ";
 
+if(isset($_GET['search'])) {
+    $sql = $sql . ' where customers.first_name like :first_name';
+}
+
 $statement = $pdo->prepare($sql);
+
+if(isset($_GET['search'])){
+    $boundSearchParam = '%' . $_GET['search'] . '%';
+    $statement->bindParam(':first_name', $boundSearchParam);   
+}
 $statement->execute();
 $invoices = $statement->fetchAll(PDO::FETCH_OBJ);
 
 // var_dump($invoices);
 ?>
 
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Date</th>
-            <th>Total</th>
-            <th>Customer</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php foreach ($invoices as $invoice) : ?>
+<form action="index.php" method="GET">
+    <input
+        type="text"
+        name="search"
+        placeholder="Search by first name"
+        value="<?php echo isset($_GET['search']) ? $_GET['search'] : '' ?>"
+    >
+    <button>Search</button>
+    <a href="/">Clear</a>
+</form>
+
+<?php if(count($invoices) === 0) : ?>
+    <div>
+        No results
+    </div>
+
+<?php else: ?>
+    <table>
+        <thead>
             <tr>
-                <td>
-                    <?php echo $invoice->id ?>
-                </td>
-                <td>
-                    <?php echo $invoice->invoice_date ?>
-                </td>
-                <td>
-                    <?php echo $invoice->total ?>
-                </td>
-                <td>
-                    <?php echo "{$invoice->first_name} {$invoice->last_name}" ?>
-                </td>
+                <th>ID</th>
+                <th>Date</th>
+                <th>Total</th>
+                <th>Customer</th>
             </tr>
-        <?php endforeach; ?>
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            <?php foreach ($invoices as $invoice) : ?>
+                <tr>
+                    <td>
+                        <?php echo $invoice->id ?>
+                    </td>
+                    <td>
+                        <?php echo $invoice->invoice_date ?>
+                    </td>
+                    <td>
+                        <?php echo $invoice->total ?>
+                    </td>
+                    <td>
+                        <?php echo "{$invoice->first_name} {$invoice->last_name}" ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php endif; ?>
